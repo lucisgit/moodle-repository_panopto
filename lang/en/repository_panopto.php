bf47f477a8de3541ea84299e019d75d3f2f56192
Ruslan Kabalin <r.kabalin@lancaster.ac.uk>
2017-03-14 17:21:33 +0000

    repository_panopto: Add plugin skeleton.

---
+++ lang/en/repository_panopto.php
@@ -0,0 +1,38 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file definitions for Panopto repository plugin.
+ *
+ * @package    repository_panopto
+ * @copyright  2017 Lancaster University (http://www.lancaster.ac.uk/)
+ * @author     Ruslan Kabalin (https://github.com/kabalin)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['applicationkey'] = 'Identitiy Provider Application Key';
+$string['applicationkeydesc'] = 'Application Key from Panopto Identity Providers settings, e.g. \'00000000-0000-0000-0000-000000000000\'.';
+$string['configplugin'] = 'Panopto configuration';
+$string['instancename'] = 'Identitiy Provider Instance Name';
+$string['instancenamedesc'] = 'Instance name from Panopto Identity Providers settings.';
+$string['password'] = 'Panopto API user password';
+$string['passworddesc'] = 'Password for API user authentication.';
+$string['pluginname'] = 'Panopto';
+$string['userkey'] = 'Panopto API username';
+$string['userkeydesc'] = 'User on the Panopto server to use for API calls, it needs to have Administrator rights.';
+$string['serverhostname'] = 'Panopto server hostname';
+$string['serverhostnamedesc'] = 'FQDN of your Panopto server, e.g. \'demo.hosted.panopto.com\'.';
---
+++ lib.php
@@ -0,0 +1,137 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Panopto repository plugin.
+ *
+ * @package    repository_panopto
+ * @copyright  2017 Lancaster University (http://www.lancaster.ac.uk/)
+ * @author     Ruslan Kabalin (https://github.com/kabalin)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . "/local/panopto/lib/panopto/lib/Client.php");
+require_once($CFG->dirroot . '/repository/lib.php');
+
+/**
+ * Repository plugin for accessing Panopto files.
+ *
+ * @package    repository_panopto
+ * @copyright  2017 Lancaster University (http://www.lancaster.ac.uk/)
+ * @author     Ruslan Kabalin (https://github.com/kabalin)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class repository_panopto extends repository {
+    /**
+     * Given a path, and perhaps a search, get a list of files.
+     *
+     * @param string $path identifier for current path.
+     * @param string $page the page number of file list.
+     * @return array list of files including meta information as specified by parent.
+     */
+    public function get_listing($path = '', $page = '') {
+        return array('list' => array());
+    }
+
+    /**
+     * Return names of the options to display in the repository plugin config form.
+     *
+     * @return array of option names.
+     */
+    public static function get_type_option_names() {
+        return array('serverhostname', 'userkey', 'password', 'instancename', 'applicationkey', 'pluginname');
+    }
+
+    /**
+     * Setup repistory form.
+     *
+     * @param moodleform $mform Moodle form (passed by reference).
+     * @param string $classname repository class name.
+     */
+    public static function type_config_form($mform, $classname = 'repository') {
+        parent::type_config_form($mform);
+        $strrequired = get_string('required');
+
+        // Server hostname.
+        $mform->addElement('text', 'serverhostname', get_string('serverhostname', 'repository_panopto'));
+        $mform->addRule('serverhostname', $strrequired, 'required', null, 'client');
+        $mform->setType('serverhostname', PARAM_HOST);
+        $mform->addElement('static', 'serverhostnamedesc', '', get_string('serverhostnamedesc', 'repository_panopto'));
+
+        // User key.
+        $mform->addElement('text', 'userkey', get_string('userkey', 'repository_panopto'));
+        $mform->addRule('userkey', $strrequired, 'required', null, 'client');
+        $mform->setType('userkey', PARAM_RAW_TRIMMED);
+        $mform->addElement('static', 'userkeydesc', '', get_string('userkeydesc', 'repository_panopto'));
+
+        // Password.
+        $mform->addElement('text', 'password', get_string('password', 'repository_panopto'));
+        $mform->addRule('password', $strrequired, 'required', null, 'client');
+        $mform->setType('password', PARAM_RAW_TRIMMED);
+        $mform->addElement('static', 'passworddesc', '', get_string('passworddesc', 'repository_panopto'));
+
+        // Instance name.
+        $mform->addElement('text', 'instancename', get_string('instancename', 'repository_panopto'));
+        $mform->addRule('instancename', $strrequired, 'required', null, 'client');
+        $mform->setType('instancename', PARAM_RAW_TRIMMED);
+        $mform->addElement('static', 'instancenamedesc', '', get_string('instancenamedesc', 'repository_panopto'));
+
+        // Application key.
+        $mform->addElement('text', 'applicationkey', get_string('applicationkey', 'repository_panopto'));
+        $mform->addRule('applicationkey', $strrequired, 'required', null, 'client');
+        $mform->setType('applicationkey', PARAM_RAW_TRIMMED);
+        $mform->addElement('static', 'applicationkeydesc', '', get_string('applicationkeydesc', 'repository_panopto'));
+    }
+
+    /**
+     * This repository doesn't support global search.
+     *
+     * @return bool if supports global search
+     */
+    public function global_search() {
+        return false;
+    }
+
+    /**
+     * This repository supports any filetype.
+     *
+     * @return string '*' means this repository support any files
+     */
+    public function supported_filetypes() {
+        return '*';
+    }
+
+    /**
+     * This repository only supports external files
+     *
+     * @return int return type bitmask supported
+     */
+    public function supported_returntypes() {
+        return FILE_EXTERNAL;
+    }
+
+    /**
+     * We do not treat Panopto site data as private.
+     *
+     * @return bool
+     */
+    public function contains_private_data() {
+        return false;
+    }
+}
